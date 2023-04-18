885ffdd05f54aafc0469c0d372d35b417c15f970
zagui-master <github.correo@gmail.com>
2023-04-18 10:17:05 -0500

    add a new urls in urls.js file

--- js/helpers/urls.js
+++ js/helpers/urls.js
@@ -1,5 +1,5 @@
 /** @format */
-
+/*
 export const url = {
   log_in: "http://127.0.0.1:5502/src/index.html",
   sign_up: "http://127.0.0.1:5502/src/pages/sign-up.html",
@@ -7,7 +7,17 @@ export const url = {
   payment: "http://127.0.0.1:5502/src/pages/payment.html",
   admin: "http://127.0.0.1:5502/src/pages/admy.html",
 };
+*/
+export const url = {
+  log_in: "index.html",
+  sign_up: "../pages/sign-up.html",
+  courses: "../pages/courses.html",
+  payment: "../pages/payment.html",
+  admin: "../pages/admy.html",
+};
 
 //log_in: "../index.html",
 //sign_up: "./pages/sign_up.html",
 //courses: "./courses.html",
+
+//https://zagui-master.github.io/Qasoft.github.io/
